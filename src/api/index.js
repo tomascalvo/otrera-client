a20bad7d6367782b7089480dde08640ddcce5445
tomascalvo <tomasrcalvo@gmail.com>
2021-11-07 17:15:39 -0500

    Changed api baseURL to heroku url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,8 @@
 
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://git.heroku.com/otrera-server.git" });
 
 const EDB = axios.create(
   { 
