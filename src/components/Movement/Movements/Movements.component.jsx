2525fc3384690eb3627099bfebd7a4c593b9b91d
tomascalvo <tomasrcalvo@gmail.com>
2021-11-18 18:18:00 -0500

    Added CircularProgress to Movements.component

--- src/components/Movement/Movements/Movements.component.jsx
+++ src/components/Movement/Movements/Movements.component.jsx
@@ -5,14 +5,19 @@ import useStyles from "./Movements.styles";
 
 // components
 
-import { Container, Typography, Grid } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Grid,
+  Box,
+  CircularProgress,
+} from "@mui/material";
 
 import Header from "../../Header/Header.component";
 import MovementPicker from "./MovementPicker/MovementPicker.component";
 import MovementCard from "./MovementCard/MovementCard.component";
 
 const Movements = () => {
-  
   // hooks
 
   const theme = useTheme();
@@ -20,7 +25,7 @@ const Movements = () => {
 
   // state
 
-  const defaultMovements = [];
+  const defaultMovements = "loading...";
   const [movements, setMovements] = useState(defaultMovements);
 
   return (
@@ -37,23 +42,46 @@ const Movements = () => {
         <Typography variant="h5" color="text.secondary" gutterBottom>
           Search Results:
         </Typography>
-        <Grid container spacing={4}>
-          {movements.map((movement, i) => (
-            <Grid
-              item
-              xs={6}
-              sm={4}
-              md={3}
-              xl={3}
-              key={i}
-              styles={{
-                height: "100%",
-              }}
-            >
-              <MovementCard key={i} cardIndex={i} movement={movement} setMovements={setMovements} />
+        {movements === "loading..." ? (
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Grid item>
+              <CircularProgress style={{ margin: "150px auto" }} />
             </Grid>
-          ))}
-        </Grid>
+          </Grid>
+        ) : movements.length > 0 ? (
+          <Grid container spacing={4}>
+            {movements.map((movement, i) => (
+              <Grid
+                item
+                xs={6}
+                sm={4}
+                md={3}
+                xl={3}
+                key={i}
+                styles={{
+                  height: "100%",
+                }}
+              >
+                <MovementCard
+                  key={i}
+                  cardIndex={i}
+                  movement={movement}
+                  setMovements={setMovements}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <Box>
+            <Typography variant="body1">No movements found.</Typography>
+          </Box>
+        )}
       </Container>
     </>
   );
