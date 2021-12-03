683fae7d6c8c0a5c11635073a5f643e9cb1dbf8c
tomascalvo <tomasrcalvo@gmail.com>
2021-12-03 13:26:43 -0500

    Reduced top padding on Dashboard container

--- src/components/Dashboard/Dashboard.styles.js
+++ src/components/Dashboard/Dashboard.styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles((theme) => ({
   container: {
     // backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
+    padding: theme.spacing(0, 0, 6),
   },
   mobileContainer: {
     backgroundColor: theme.palette.background.paper,
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@mui/styles";
 export default makeStyles((theme) => ({
   box: {
     bgcolor: theme.palette.background.paper,
-    padding: theme.spacing(0, 0, 2),
+    padding: theme.spacing(8, 0, 2),
     [theme.breakpoints.down('sm')]: {
       padding: theme.spacing(2, 1, 1),
     },
