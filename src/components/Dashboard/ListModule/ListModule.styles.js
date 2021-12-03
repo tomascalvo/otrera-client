6f75dbb3621f2abca5f49c1958e1cd19df120a31
tomascalvo <tomasrcalvo@gmail.com>
2021-12-03 13:23:35 -0500

    Removed border: '1px solid red' from ListModule flexContainer

--- src/components/Dashboard/ListModule/ListModule.styles.js
+++ src/components/Dashboard/ListModule/ListModule.styles.js
@@ -2,10 +2,7 @@ import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
   flexContainer: {
-    // height: `calc(100% - ${theme.spacing(4)})`,
     height: '100%',
-    // height: 'auto',
-    border: '1px solid red',
     padding: 0,
     display: "flex",
     flexDirection: "column",
@@ -13,7 +10,6 @@ const useStyles = makeStyles((theme) => ({
   },
   buttonDiv: {
     margin: "0",
-    // height: "100%",
     display: "flex",
     justifyContent: "flex-end",
   },
